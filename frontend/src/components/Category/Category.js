62d722b6219561ba8ffb303b1e2f90beb7913228
Javiera <49495525+JaviNGD@users.noreply.github.com>
2024-02-23 12:32:41 -0300

    Add styling to categories

--- frontend/src/components/Category/Category.js
+++ frontend/src/components/Category/Category.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import categoryClass from './category.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Category({categories}) {
+  // Get the current location. 
+  const location = useLocation();
+
   return (
     <div className={categoryClass.container}>
         {
             categories.map(category => (
-                <Link key={category.name} to={`/category/${category.name}`}>
+                <Link key={category.name} to={`/category/${category.name}`} className={location.pathname === `/category/${category.name}` ? categoryClass.activeCategory : ''}>
                     {category.name} ({category.count})
                 </Link>
         ))}
